651545e61b54c2bb7c00808e76b495e678530b07
Lars Kellogg-Stedman <lars@oddbit.com>
2015-10-22 09:55:50 -0400

    inc version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt') as fd:
           author='Lars Kellogg-Stedman',
           author_email='lars@oddbit.com',
           url='https://github.com/larsks/opensink',
-          version='0.2',
+          version='0.3',
           packages=find_packages(),
           install_requires=requires,
           )
